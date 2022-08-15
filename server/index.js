d99b4b819d3e6958f95c1852f7e581d2c29cb0e5
938938 <938kjh@gmail.com>
2022-08-15 22:59:28 +0900

    기본 채팅 기능 구현

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const port = 4050;
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} 유저가 접속했습니다`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`${socket.id}유저가 ${data}번 방에 입장했습니다`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+    console.log('데이터 전송')
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(port, () => console.log(`server running on port ${port}`));
