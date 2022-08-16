3cee3ef843ff1d188fe38ebe667aa0a0324ef24f
938938 <938kjh@gmail.com>
2022-08-16 23:37:06 +0900

    styled-components 변경 중

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
 import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./components/Chat";
+import styled from "styled-components";
 
 const socket = io.connect("http://localhost:4050");
 
@@ -18,31 +18,79 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <ChatApp>
       {!showChat ? (
-        <div className="ChatContainer">
-          <h3>Chat !</h3>
-          <input
+        <ChatContainer>
+          <ChatTitle>Chat !</ChatTitle>
+          <ChatInput
             type="text"
             placeholder="사용할 이름을 입력해주세요"
             onChange={(e) => {
               setUsername(e.target.value);
             }}
           />
-          <input
+          <ChatInput
             type="text"
             placeholder="입장할 방을 입력해주세요"
             onChange={(e) => {
               setRoom(e.target.value);
             }}
           />
-          <button onClick={joinRoom}>입장</button>
-        </div>
+          <ChatButton onClick={joinRoom}>입장</ChatButton>
+        </ChatContainer>
       ) : (
         <Chat socket={socket} username={username} room={room} />
       )}
-    </div>
+    </ChatApp>
   );
 }
 
 export default App;
+
+const ChatApp = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #fff;
+  color: #212121;
+  font-family: "Open Sans", sans-serif;
+  display: grid;
+  place-items: center;
+`
+
+const ChatContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  border:1px solid steelblue;
+  border-radius:6px;
+  padding:10px;
+`
+const ChatTitle = styled.h3`
+  font-size: 2rem;
+  margin-bottom: 1rem;
+  color:steelblue;
+`
+const ChatInput = styled.input`
+  height: 35px;
+  margin: 7px;
+  border: 2px solid steelblue;
+  border-radius: 5px;
+  padding: 5px;
+  font-size: 16px;
+`
+
+const ChatButton = styled.button`
+  width: 200px;
+  height: 50px;
+  margin: 10px auto;
+  border: none;
+  border-radius: 5px;
+  padding: 5px;
+  font-size: 16px;
+  background: steelblue;
+  color: #fff;
+  cursor: pointer;
+  &:hover{  
+    background: rgb(35, 65, 89);
+  }
+`
--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import GlobalStyle from './GlobalStyle';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <GlobalStyle> */}
+      <App />
+    {/* </GlobalStyle> */}
   </React.StrictMode>
 );
 
